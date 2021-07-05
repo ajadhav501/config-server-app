3bbdbdc2d2a1fe8d72dc62bbceabb02e6b164399
Abhijeet Jadhav <ajadhav50@yahoo.com>
2021-07-05 19:10:11 +0530

    first commit for config server

---
+++ src/test/java/com/epam/gpop/configserverapp/ConfigServerAppApplicationTests.java
@@ -0,0 +1,15 @@
+package com.epam.gpop.configserverapp;
+
+import org.junit.Ignore;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Ignore
+class ConfigServerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
